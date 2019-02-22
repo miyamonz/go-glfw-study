53675b54be8eac6d10b6ad65e5fadeda93b5cfa9
Koichi Miyamoto <m5813k@gmail.com>
2019-02-22 16:35:50 +0900

    BufferDataとVertexAttribPointerで,型のサイズを取得して渡す

--- object.go
+++ object.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/go-gl/gl/v4.1-core/gl"
+	"unsafe"
 )
 
 type Object struct {
@@ -18,14 +19,16 @@ func createVBO(data []Vec3) uint32 {
 	var vbo uint32
 	gl.GenBuffers(1, &vbo)
 	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
-	size := len(data) * len(data[0])
-	gl.BufferData(gl.ARRAY_BUFFER, size*4, gl.Ptr(data), gl.STATIC_DRAW)
+	num := len(data)
+	vertexSize := int(unsafe.Sizeof(data[0]))
+	gl.BufferData(gl.ARRAY_BUFFER, num*vertexSize, gl.Ptr(data), gl.STATIC_DRAW)
 	return vbo
 }
 func createIBO(index []uint32) uint32 {
 	var ibo uint32
 	gl.GenBuffers(1, &ibo)
 	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo)
+	// 4はsizeof(uint32)ということ. 32bitは4byte
 	gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, len(index)*4, gl.Ptr(index), gl.STATIC_DRAW)
 	return ibo
 }
@@ -41,7 +44,11 @@ func NewObject(points []Vec3) Object {
 		vbo: createVBO(points),
 	}
 
-	gl.VertexAttribPointer(0, size, gl.FLOAT, false, 0, gl.PtrOffset(0))
+	// Vec3なら要素数3と各要素32bit == 4byte なので12のはず
+	vertexSize := int32(unsafe.Sizeof(points[0]))
+
+	//func VertexAttribPointer(index uint32, size int32, xtype uint32, normalized bool, stride int32, pointer unsafe.Pointer)
+	gl.VertexAttribPointer(0, size, gl.FLOAT, false, vertexSize, gl.PtrOffset(0))
 	gl.EnableVertexAttribArray(0)
 
 	return obj
