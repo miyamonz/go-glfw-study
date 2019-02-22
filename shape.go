5adaf68b156b9ab05ac73c907231e0024129a64f
Koichi Miyamoto <m5813k@gmail.com>
2019-02-22 19:45:48 +0900

    面ごとに色付け
    indexを使わないで、正方形一面を3 + 3頂点で表現するやつは、飛ばす

--- shape.go
+++ shape.go
@@ -92,13 +92,61 @@ func NewCube() ShapeIndex {
 }
 
 func NewSolidCube() ShapeIndex {
+	var corners []Vec3 = []Vec3{
+		Vec3{-1.0, -1.0, -1.0},
+		Vec3{-1.0, -1.0, 1.0},
+		Vec3{-1.0, 1.0, 1.0},
+		Vec3{-1.0, 1.0, -1.0},
+		Vec3{1.0, 1.0, -1.0},
+		Vec3{1.0, -1.0, -1.0},
+		Vec3{1.0, -1.0, 1.0},
+		Vec3{1.0, 1.0, 1.0},
+	}
+	red := Vec3{0.8, 0.1, 0.1}
+	green := Vec3{0.1, 0.8, 0.1}
+	blue := Vec3{0.1, 0.1, 0.8}
+	yellow := Vec3{0.8, 0.8, 0.1}
+	magenta := Vec3{0.8, 0.1, 0.8}
+	cyan := Vec3{0.1, 0.8, 0.8}
+	var cubeVertex []Vertex = []Vertex{
+		// 左
+		NewVertex(corners[0], green),
+		NewVertex(corners[1], green),
+		NewVertex(corners[2], green),
+		NewVertex(corners[3], green),
+		// 裏
+		NewVertex(corners[5], magenta),
+		NewVertex(corners[0], magenta),
+		NewVertex(corners[3], magenta),
+		NewVertex(corners[4], magenta),
+		// 下
+		NewVertex(corners[0], cyan),
+		NewVertex(corners[5], cyan),
+		NewVertex(corners[6], cyan),
+		NewVertex(corners[1], cyan),
+		// 右
+		NewVertex(corners[6], blue),
+		NewVertex(corners[5], blue),
+		NewVertex(corners[4], blue),
+		NewVertex(corners[7], blue),
+		// 上
+		NewVertex(corners[3], red),
+		NewVertex(corners[2], red),
+		NewVertex(corners[7], red),
+		NewVertex(corners[4], red),
+		// 前
+		NewVertex(corners[1], yellow),
+		NewVertex(corners[6], yellow),
+		NewVertex(corners[7], yellow),
+		NewVertex(corners[2], yellow),
+	}
 	var solidCubeIndex []uint32 = []uint32{
 		0, 1, 2, 0, 2, 3, // 左
-		0, 3, 4, 0, 4, 5, // 裏
-		0, 5, 6, 0, 6, 1, // 下
-		7, 6, 5, 7, 5, 4, // 右
-		7, 4, 3, 7, 3, 2, // 上
-		7, 2, 1, 7, 1, 6, // 前
+		4, 5, 6, 4, 6, 7, // 裏
+		8, 9, 10, 8, 10, 11, //下
+		12, 13, 14, 12, 14, 15, // 右
+		16, 17, 18, 16, 18, 19, // 上
+		20, 21, 22, 20, 22, 23, //前
 	}
 	return NewSolidShapeIndex(cubeVertex, solidCubeIndex)
 }
